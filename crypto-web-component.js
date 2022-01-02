da476e8304ef5fe91ff6f9623b901ff4184269f1
Ben Brown <bdbrown4@eagles.usi.edu>
2022-01-01 21:07:20 -0500

    upgraded to dynamic datalist... figure out cleaning up template and have it live in html on its own...

--- crypto-web-component.js
+++ crypto-web-component.js
@@ -6,44 +6,62 @@
 //     .then(text => define(text));
 const template = `
 <body>
-    <select id="cryptocurrency" type="text">
-        <option value=""></option>
-        <option value="BTC">BTC</option>
-        <option value="ETH">ETH</option>
-        <option value="LTC">LTC</option>
-    </select>
-    <button disabled="true" id="fetchCryptoValue">Fetch a cryptocurrency</button>
-    <div id="cryptovalue" hidden="true">
+    <input list='cryptocurrencies' id='cryptocurrency'>
+    <datalist id='cryptocurrencies'>
+    </datalist>
+    <button disabled='true' id='fetchCryptoValue'>Fetch a cryptocurrency</button>
+    <div id='cryptovalue' hidden='true'>
     </div>
 </body>`;
-define(template);
 /*  
     Never ever do this in a production scenario where you need to protect your endpoints
     from nurds.
 */
-const apiKey = '29416EA8-E343-43F4-A5DC-6C360653D6A4';
+apiKey = '29416EA8-E343-43F4-A5DC-6C360653D6A4';
+define(template);
 
 function define(html) {
     class CryptoWebComponent extends HTMLElement {
-        cryptoCurrency = "";
+        cryptoCurrency = '';
         constructor() {
             super();
             this.attachShadow({ mode: 'open' });
             this.shadowRoot.innerHTML = html;
+            this.fetchDataListOptions();
         }
     
         connectedCallback() {
             this.shadowRoot.querySelector('#fetchCryptoValue')
                 .addEventListener('click', this.fetchCryptoCurrency.bind(this));
-            this.shadowRoot.querySelector("#cryptocurrency")
-                .addEventListener('change', this.activateButton.bind(this));
+            this.shadowRoot.querySelector('#cryptocurrency')
+                .addEventListener('input', this.activateButton.bind(this));
         }
     
         disconnectedCallback() {
             this.shadowRoot.querySelector('#fetchCryptoValue')
                 .removeEventListener();
-            this.shadowRoot.querySelector("#cryptocurrency")
+            this.shadowRoot.querySelector('#cryptocurrency')
                 .removeEventListener();
+        }
+
+        async fetchDataListOptions() {
+            var uri = 'https://rest.coinapi.io/v1/assets';
+            var response = await fetch(uri, {
+                method: 'GET',
+                headers: {
+                    'X-CoinAPI-Key': apiKey
+                }
+            }).then(data => data.json());
+
+            var datalist = this.shadowRoot.querySelector('#cryptocurrencies');
+            console.log(datalist);
+
+            response.forEach(crypto => {
+                const option = document.createElement('option');
+                option.value = crypto.asset_id;
+                option.innerText = `${crypto.name} - ${crypto.asset_id}`;
+                datalist.appendChild(option);
+            });
         }
     
         async fetchCryptoCurrency() {
@@ -55,23 +73,25 @@ function define(html) {
                 }
             }).then(data => data.json());
 
-            const div = this.shadowRoot.querySelector("#cryptovalue");
-            const para = document.createElement("p");
-            para.innerHTML = 'USD: $'+(Math.round(response.rate * 100) / 100).toLocaleString();
+            const div = this.shadowRoot.querySelector('#cryptovalue');
+            const para = document.createElement('p');
+            para.innerHTML = response.error ?
+                `There was an error fetching ${this.cryptoCurrency} from CoinAPI.` :
+                'USD: $'+(Math.round(response.rate * 100) / 100).toLocaleString();
             div.appendChild(para);
             div.hidden = false;
         }
 
         activateButton() {
-            const div = this.shadowRoot.querySelector("#cryptovalue");
-            div.innerHTML = "";
+            const div = this.shadowRoot.querySelector('#cryptovalue');
+            div.innerHTML = '';
             div.hidden = true;
-            const selection = this.shadowRoot.querySelector("#cryptocurrency");
-            this.cryptoCurrency = selection.options[selection.selectedIndex].value;
+            const selection = this.shadowRoot.querySelector('#cryptocurrency');
+            this.cryptoCurrency = selection.value;
             if (this.cryptoCurrency) {
-                this.shadowRoot.querySelector("#fetchCryptoValue").disabled = false;
+                this.shadowRoot.querySelector('#fetchCryptoValue').disabled = false;
             } else {
-                this.shadowRoot.querySelector("#fetchCryptoValue").disabled = true;
+                this.shadowRoot.querySelector('#fetchCryptoValue').disabled = true;
             }
         }
     }
